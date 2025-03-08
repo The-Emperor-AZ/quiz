d8f64d7b8ff6e1bc56c2b759075d69c9f9703e35
Emperor <luiferrojas61@gmail.com>
2025-03-07 20:09:42 -0500

    confuse

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,20 +1,21 @@
-
 import java.util.Scanner;
-
 public class Ejercicio1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner("System.in");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la contraseña");
-        short contraseña = scanner.nextShort();
+        String contraseña = scanner.nextLine();
 
-        if (contraseña == '@' || contraseña == '#' || contraseña == '$' || contraseña == '%' || contraseña == '&' || contraseña == '*')
-        System.out.println("la contraseña es valida"); {
-        
-            
+        if (contraseña.length() >= 8 &&
+            contraseña.matches(".*[A-Z].*") &&
+            contraseña.matches(".*[a-z].*") &&
+            contraseña.matches(".*[0-9].*") &&
+            contraseña.matches(".*[!@#$%^&*].*")) {
+            System.out.println("La contraseña es válida");
+        } else {
+            System.out.println("La contraseña debe tener al menos 8 caracteres, incluir una letra mayúscula, una minúscula, un número y un carácter especial (@, #, $, %, &, ).");
         }
 
-         scanner.close();   
-        }
+        scanner.close();
     }
-    
+}
 
